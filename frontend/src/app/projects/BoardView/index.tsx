4cf09f8a69c438bf9b9cde11d26304358d3ffda8
Amiel Brencis Salipande <absalipande@gmail.com>
2024-11-05 16:49:40 +0800

    feat: Applied fix

--- frontend/src/app/projects/BoardView/index.tsx
+++ frontend/src/app/projects/BoardView/index.tsx
@@ -162,13 +162,7 @@ const Task = ({ task }: TaskProps) => {
       className={`mb-4 rounded-md bg-white shadow dark:bg-dark-secondary ${isDragging ? 'opacity-50' : 'opacity-100'}`}
     >
       {task.attachments && task.attachments.length > 0 && (
-        <Image
-          src={`https://minerva-s3-images.s3.ap-southeast-1.amazonaws.com/${task.attachments[0].fileURL}`}
-          alt={task.attachments[0].fileName}
-          width={400}
-          height={200}
-          className='h-auto w-full rounded-t-md'
-        />
+        <Image src={`/${task.attachments[0].fileURL}`} alt={task.attachments[0].fileName} width={400} height={200} className='h-auto w-full rounded-t-md' />
       )}
 
       <div className='p-4 md:p-6'>
@@ -206,7 +200,7 @@ const Task = ({ task }: TaskProps) => {
           <div className='flex -space-x-[6px] overflow-hidden'>
             {task.assignee && (
               <Image
-                src={`https://minerva-s3-images.s3.ap-southeast-1.amazonaws.com/${task.assignee.profilePictureUrl!}`}
+                src={`/${task.assignee.profilePictureUrl!}`}
                 key={task.assignee.userId}
                 alt={task.assignee.username}
                 width={30}
@@ -216,7 +210,7 @@ const Task = ({ task }: TaskProps) => {
             )}
             {task.author && (
               <Image
-                src={`https://minerva-s3-images.s3.ap-southeast-1.amazonaws.com/${task.author.profilePictureUrl!}`}
+                src={`/${task.author.profilePictureUrl!}`}
                 key={task.author.userId}
                 alt={task.author.username}
                 width={30}
--- frontend/src/components/Sidebar/index.tsx
+++ frontend/src/components/Sidebar/index.tsx
@@ -49,7 +49,7 @@ const Sidebar = () => {
 
         {/* TEAM */}
         <div className='flex items-center gap-5 border-y-[1.5px] border-zinc-200 px-8 py-4 dark:border-zinc-700'>
-          <Image src='https://minerva-s3-images.s3.ap-southeast-1.amazonaws.com/logo.png' alt='Logo' width={40} height={40} />
+          <Image src='/logo.png' alt='Logo' width={40} height={40} />
           <div>
             <h3 className='text-md font-bold tracking-wide dark:text-gray-200'>Minerva Team</h3>
             <div className='mt-1 flex items-start gap-2'>
