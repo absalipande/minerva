0b7617c2e3e78961645728bbdd57a8c45a5ca780
Amiel Brencis Salipande <absalipande@gmail.com>
2024-11-05 20:27:43 +0800

    feat: Some small UI updates

--- frontend/src/app/priority/reusablePriorityPage/index.tsx
+++ frontend/src/app/priority/reusablePriorityPage/index.tsx
@@ -1,14 +1,14 @@
-"use client";
+'use client';
 
-import { useAppSelector } from "@/app/redux";
-import Header from "@/components/Header";
-import ModalNewTask from "@/components/ModalNewTask";
-import TaskCard from "@/components/TaskCard";
-import { dataGridClassNames, dataGridSxStyles } from "@/lib/utils";
-import { Priority, Task, useGetTasksByUserQuery } from "@/state/api";
-import { DataGrid, GridColDef } from "@mui/x-data-grid";
-import { Loader2 } from "lucide-react";
-import React, { useState } from "react";
+import { useAppSelector } from '@/app/redux';
+import Header from '@/components/Header';
+import ModalNewTask from '@/components/ModalNewTask';
+import TaskCard from '@/components/TaskCard';
+import { dataGridClassNames, dataGridSxStyles } from '@/lib/utils';
+import { Priority, Task, useGetTasksByUserQuery } from '@/state/api';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import { Loader2 } from 'lucide-react';
+import React, { useState } from 'react';
 
 type Props = {
   priority: Priority;
@@ -16,153 +16,117 @@ type Props = {
 
 const columns: GridColDef[] = [
   {
-    field: "title",
-    headerName: "Title",
+    field: 'title',
+    headerName: 'Title',
     width: 100,
   },
   {
-    field: "description",
-    headerName: "Description",
+    field: 'description',
+    headerName: 'Description',
     width: 200,
   },
   {
-    field: "status",
-    headerName: "Status",
+    field: 'status',
+    headerName: 'Status',
     width: 130,
-    renderCell: (params) => (
-      <span className="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800">
-        {params.value}
-      </span>
-    ),
+    renderCell: (params) => <span className='inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800'>{params.value}</span>,
   },
   {
-    field: "priority",
-    headerName: "Priority",
+    field: 'priority',
+    headerName: 'Priority',
     width: 75,
   },
   {
-    field: "tags",
-    headerName: "Tags",
+    field: 'tags',
+    headerName: 'Tags',
     width: 130,
   },
   {
-    field: "startDate",
-    headerName: "Start Date",
+    field: 'startDate',
+    headerName: 'Start Date',
     width: 130,
   },
   {
-    field: "dueDate",
-    headerName: "Due Date",
+    field: 'dueDate',
+    headerName: 'Due Date',
     width: 130,
   },
   {
-    field: "author",
-    headerName: "Author",
+    field: 'author',
+    headerName: 'Author',
     width: 150,
-    renderCell: (params) => params.value.username || "Unknown",
+    renderCell: (params) => params.value.username || 'Unknown',
   },
   {
-    field: "assignee",
-    headerName: "Assignee",
+    field: 'assignee',
+    headerName: 'Assignee',
     width: 150,
-    renderCell: (params) => params.value.username || "Unassigned",
+    renderCell: (params) => params.value.username || 'Unassigned',
   },
 ];
 
 const ReusablePriorityPage = ({ priority }: Props) => {
-  const [view, setView] = useState("list");
+  const [view, setView] = useState('list');
   const [isModalNewTaskOpen, setIsModalNewTaskOpen] = useState(false);
 
   const userId = 1;
-  const {
-    data: tasks,
-    isLoading,
-    isError: isTasksError,
-  } = useGetTasksByUserQuery(userId || 0, { skip: userId === null });
+  const { data: tasks, isLoading, isError: isTasksError } = useGetTasksByUserQuery(userId || 0, { skip: userId === null });
 
   const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
 
-  const filteredTasks = tasks?.filter(
-    (task: Task) => task.priority === priority,
-  );
+  const filteredTasks = tasks?.filter((task: Task) => task.priority === priority);
 
   if (isLoading) {
     return (
-      <div className="flex h-[calc(100vh-200px)] items-center justify-center">
-        <Loader2 className="h-8 w-8 animate-spin text-blue-500" />
+      <div className='flex h-[calc(100vh-200px)] items-center justify-center'>
+        <Loader2 className='h-8 w-8 animate-spin text-blue-500' />
       </div>
     );
   }
 
   return (
-    <div className="m-5 p-4">
-      <ModalNewTask
-        isOpen={isModalNewTaskOpen}
-        onClose={() => setIsModalNewTaskOpen(false)}
-      />
+    <div className='m-5 p-4'>
+      <ModalNewTask isOpen={isModalNewTaskOpen} onClose={() => setIsModalNewTaskOpen(false)} />
       <Header
-        name="Priority Page"
+        name='Priority Page'
         buttonComponent={
-          <button
-            className="mr-3 rounded bg-zinc-700 px-4 py-2 font-bold text-white hover:bg-zinc-600"
-            onClick={() => setIsModalNewTaskOpen(true)}
-          >
+          <button className='mr-3 rounded bg-zinc-700 px-4 py-2 font-bold text-white hover:bg-zinc-600' onClick={() => setIsModalNewTaskOpen(true)}>
             Add Task
           </button>
         }
       />
-      <div className="mb-4 flex justify-start">
-        <button
-          className={`px-4 py-2 ${view === "list" ? "bg-gray-300" : "bg-white"} rounded-l`}
-          onClick={() => setView("list")}
-        >
+      <div className='mb-4 flex justify-start gap-2'>
+        <button className={`px-4 py-2 ${view === 'list' ? 'bg-gray-300' : 'bg-white'} rounded`} onClick={() => setView('list')}>
           List
         </button>
-        <button
-          className={`px-4 py-2 ${view === "table" ? "bg-gray-300" : "bg-white"} rounded-l`}
-          onClick={() => setView("table")}
-        >
+        <button className={`px-4 py-2 ${view === 'table' ? 'bg-gray-300' : 'bg-white'} rounded`} onClick={() => setView('table')}>
           Table
         </button>
       </div>
 
       {/* Task Rendering */}
       {filteredTasks && filteredTasks.length > 0 ? (
-        view === "list" ? (
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        view === 'list' ? (
+          <div className='grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
             {filteredTasks.map((task: Task) => (
-              <TaskCard key={task.id} task={task} className="h-64 w-64" />
+              <TaskCard key={task.id} task={task} className='h-64 w-64' />
             ))}
           </div>
         ) : (
-          view === "table" && (
-            <div className="z-0 w-full">
-              <DataGrid
-                rows={filteredTasks}
-                columns={columns}
-                checkboxSelection
-                getRowId={(row) => row.id}
-                className={dataGridClassNames}
-                sx={dataGridSxStyles(isDarkMode)}
-              />
+          view === 'table' && (
+            <div className='z-0 w-full'>
+              <DataGrid rows={filteredTasks} columns={columns} checkboxSelection getRowId={(row) => row.id} className={dataGridClassNames} sx={dataGridSxStyles(isDarkMode)} />
             </div>
           )
         )
       ) : (
-        <div className="mt-10 flex flex-col items-center justify-center py-4">
-          <p className="mb-2 text-center font-semibold text-zinc-500 dark:text-zinc-500">
-            Nothing to see here
-          </p>
-          <img
-            src="/frog-waiting.png"
-            alt="Error"
-            className="h-64 w-64 object-cover"
-          />
+        <div className='mt-10 flex flex-col items-center justify-center py-4'>
+          <p className='mb-2 text-center font-semibold text-zinc-500 dark:text-zinc-500'>Nothing to see here</p>
+          <img src='/frog-waiting.png' alt='Error' className='h-64 w-64 object-cover' />
         </div>
       )}
     </div>
   );
 };
-
 
 export default ReusablePriorityPage;
